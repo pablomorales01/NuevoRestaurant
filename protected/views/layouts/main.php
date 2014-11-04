b30202a75eba68f8c4695ca979174786c86539bf
Pablo Morales Alarcón <pamoral@alumnos.ubiobio.cl>
2014-11-04 13:43:08 -0300

    NuevoRestaurant

---
+++ protected/views/layouts/main.php
@@ -0,0 +1,182 @@
+<?php /* @var $this Controller */ ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="language" content="en" />
+
+	<!-- blueprint CSS framework -->
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
+	<!--[if lt IE 8]>
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
+	<![endif]-->
+
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/jquery-ui-1.10.3.custom.css">
+
+	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+</head>
+
+<body>
+
+<div class="container" id="page">
+<?php echo 'contenido main' ?>
+	<div id="header">
+		
+		<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/logopenco.png" width="350" height="150">
+		<?php /*echo BsHtml::emphasis(Yii::app()->name, array(
+    		'color' => BsHtml::TEXT_ALIGN_RIGHT
+			));
+			*/?>
+		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
+		
+	</div><!-- header -->
+
+	
+
+	<!--Nuevo Menú-->
+
+	<?php
+$this->widget('bootstrap.widgets.BsNavbar', array(
+    'collapse' => true,
+    'brandLabel' => BsHtml::icon(BsHtml::GLYPHICON_HOME),
+    'brandUrl' => Yii::app()->homeUrl,
+    'items' => array(
+        array(
+            'class' => 'bootstrap.widgets.BsNav',
+            'type' => 'navbar',
+            'activateParents' => true,
+            'items' => array(
+                array(
+                    'label' => 'Home',
+                    'url' => array(
+                        '/site/index'
+                    ),
+                    'items' => array(
+                        //BOTON DROPDOWN
+                        BsHtml::menuHeader(BsHtml::icon(BsHtml::GLYPHICON_BOOKMARK), array(
+                            'class' => 'text-center',
+                            'style' => 'color:#99cc32;font-size:32px;'
+                        )),
+                        array(
+                            'label' => 'Home',
+                            'url' => array(
+                                '/site/index'
+                            )
+                        ),
+                        array(
+                            'label' => 'About',
+                            'url' => array(
+                                '/site/page',
+                                'view' => 'about'
+                            )
+                        ),
+                        array(
+                            'label' => 'Contact',
+                            'url' => array(
+                                '/site/contact'
+                            )
+                        ),
+                        BsHtml::menuDivider(),
+                        array(
+                            'label' => 'Login',
+                            'url' => array(
+                                '/site/login'
+                            ),
+                            'visible' => Yii::app()->user->isGuest,
+                            'icon' => BsHtml::GLYPHICON_LOG_IN
+                        ),
+                        array(
+                            'label' => 'Logout (' . Yii::app()->user->name . ')',
+                            'url' => array(
+                                '/site/logout'
+                            ),
+                            'visible' => !Yii::app()->user->isGuest
+                        ),
+                        array(
+                            'label' => 'Home',
+                            'url' => array(
+                                '/site/index'
+                            ),
+                            'icon' => BsHtml::GLYPHICON_HOME
+                        ),
+                        array(
+                            'label' => 'About',
+                            'url' => array(
+                                '/site/page',
+                                'view' => 'about'
+                            ),
+                            'icon' => BsHtml::GLYPHICON_PAPERCLIP
+                        ),
+                        array(
+                            'label' => 'Contact',
+                            'url' => array(
+                                '/site/contact'
+                            ),
+                            'icon' => BsHtml::GLYPHICON_FLOPPY_OPEN
+                        )
+                    )
+                )
+            )
+        ),
+        //FIN BOTON DROPDOWN
+        array(
+            'class' => 'bootstrap.widgets.BsNav',
+            'type' => 'navbar',
+            'activateParents' => true,
+            'items' => array(
+                
+                array(
+                    'label' => 'Login',
+                    'url' => array(
+                        '/site/login'
+                    ),
+                    'pull' => BsHtml::NAVBAR_NAV_PULL_RIGHT,
+                    'visible' => Yii::app()->user->isGuest
+                ),
+                array(
+                    'label' => 'Logout (' . Yii::app()->user->name . ')',
+                    'pull' => BsHtml::NAVBAR_NAV_PULL_RIGHT,
+                    'url' => array(
+                        '/site/logout'
+                    ),
+                    'visible' => !Yii::app()->user->isGuest
+                )
+            ),
+            'htmlOptions' => array(
+                'pull' => BsHtml::NAVBAR_NAV_PULL_RIGHT
+            )
+        )
+        
+    )
+));
+?>
+
+	<!--Fin Nuevo Menú-->
+	<?php if(isset($this->breadcrumbs)):?>
+		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
+			'links'=>$this->breadcrumbs,
+		)); ?><!-- breadcrumbs -->
+	<?php endif?>
+
+	<?php echo $content; ?>
+
+	<div class="clear"></div>
+
+	<div id="footer">
+		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		All Rights Reserved.<br/>
+		<?php echo Yii::powered(); ?>
+	</div><!-- footer -->
+
+</div><!-- page -->
+
+ <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery-1.9.1.min.js" type="text/javascript"></script>
+ <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/bootstrap.js" type="text/javascript"></script>
+ <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery-ui-1.10.3.custom.min.js" type="text/javascript"></script>
+ <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/demo.js" type="text/javascript"></script>
+</body>
+</html>
