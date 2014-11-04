7022dee5bdf197d891519bbac19ad1b1592c0d1f
elymarquez <marquezneira.e@gmail.com>
2014-11-04 18:39:46 -0300

    FORMULARIO OCULTO Y OTRAS COSAS ;)

--- protected/views/usuario/_form.php
+++ protected/views/usuario/_form.php
@@ -3,48 +3,73 @@
 /* @var $model Usuario */
 /* @var $form CActiveForm */
 ?>
+<?php
+$form = $this->beginWidget('bootstrap.widgets.BsActiveForm', array(
+    'layout' => BsHtml::FORM_LAYOUT_HORIZONTAL,
+    'enableAjaxValidation' => true,
+    'id' => 'user_form_horizontal',
+    'htmlOptions' => array(
+        'class' => 'bs-example'
+    )
+));
+?>
+<div class="row">
+ <div class="col-xs-12 col-sm-6 col-md-8">
 
 <?php 
 if($roles == null){
 
 
-echo BsHtml::alert(BsHtml::ALERT_COLOR_SUCCESS, BsHtml::bold('No existen Roles en el sistema. ') . 
-	'Por favor ingresa uno' . BsHtml::alertLink(' Aquí.', array(
+echo BsHtml::alert(BsHtml::ALERT_COLOR_DANGER, BsHtml::bold('No existen Roles en el sistema. ')
+	.'Por favor ingresa uno' . BsHtml::alertLink(' Aquí.', array(
     'url' => '../TipoRol/create'
 )));
-
 
 }
 
 else{
 
  ?>
-<div class="form">
 
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'usuario-form',
-	// Please note: When you enable ajax validation, make sure the corresponding
-	// controller action is handling ajax validation correctly.
-	// There is a call to performAjaxValidation() commented in generated controller code.
-	// See class documentation of CActiveForm for details on this.
-	'enableAjaxValidation'=>false,
-)); ?>
+<div class="form" >
 
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
 
 	<?php echo $form->errorSummary($model); ?>
+	<div align="left">
 
-	<div class="row">
+			<?php
+			echo $form->dropDownListControlGroup($model,'ROL_ID',array(
+				CHtml::listData(TipoRol::model()->findAll(), 'ROL_ID', 'ROLNOMBRE')),
+			array('empty' => 'Something ...')
+			);
+			echo CHtml::link('Otro Rol', array('TipoRol/create'));
+			?>			
+			</div>
+
+			<!-- MOSTRAR Y CERRAR FORMULARIO OCULTO -->
+			<a href="#" id="show">Mostrar</a>
+<div id="element" style="display: none;">
+   <div id="close"><a href="#" id="hide">cerrar</a></div>
+   <form method="post" action="">
+        Nombre:<br/>
+        <input type="text" id="name" name="name" size="40" /><br/><br/>
+        Mensaje:<br/>
+        <textarea name="message" id="message" rows="6" cols="40"></textarea>
+        <br/><br/>
+        <div style="margin-left: 376px;"><input name="submit" type="submit" value="enviar" id="enviar-btn" /></div>
+    </form>
+</div>
+
+	<!--<div class="row">
+
+
+<!-- <div class="row">
 		<?php echo $form->labelEx($model,'RESTO_ID'); ?>
 		<?php echo $form->textField($model,'RESTO_ID'); ?>
 		<?php echo $form->error($model,'RESTO_ID'); ?>
 	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'ROL_ID'); ?>
-		<?php echo $form->textField($model,'ROL_ID'); ?>
-		<?php echo $form->error($model,'ROL_ID'); ?>
-	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'USUPASSWORD'); ?>
@@ -88,13 +113,17 @@ else{
 		<?php echo $form->error($model,'USUESTADO'); ?>
 	</div>
 
+	-->
+
 	<div class="row buttons">
 		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
 	</div>
 
-<?php $this->endWidget(); 
 
-}
-?>
+
+<?php } ?>
 
 </div><!-- form -->
+</div>
+</div>
+<?php $this->endWidget();?>
