d590ec32042cf277b0c61e478f1d3aaf4384a993
Pablo Morales Alarcón <pamoral@alumnos.ubiobio.cl>
2014-11-05 23:54:52 -0300

    Vista login
    login en el centro

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -9,9 +9,11 @@ $this->breadcrumbs=array(
 );
 ?>
 
-<h1>Login</h1>
+<div class="row">
+<div class="col-xs-6 col-sm-4"></div>
+<div class="col-xs-6 col-sm-4" align="center"><h1>Bienvenido</h1>
 
-<p>Please fill out the following form with your login credentials:</p>
+<p>Por favor llene sus datos:</p>
 
 <div class="form">
 <?php $form=$this->beginWidget('CActiveForm', array(
@@ -22,7 +24,7 @@ $this->breadcrumbs=array(
 	),
 )); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+	<p class="note">Los items <span class="required">*</span> son requeridos.</p>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'username'); ?>
@@ -34,23 +36,22 @@ $this->breadcrumbs=array(
 		<?php echo $form->labelEx($model,'password'); ?>
 		<?php echo $form->passwordField($model,'password'); ?>
 		<?php echo $form->error($model,'password'); ?>
-		<p class="hint">
-			Hint: You may login with <kbd>demo</kbd>/<kbd>demo</kbd> or <kbd>admin</kbd>/<kbd>admin</kbd>.
-		</p>
+		
 	</div>
 
 	<div class="row rememberMe">
 		<?php echo $form->checkBox($model,'rememberMe'); ?>
-		<?php echo $form->label($model,'rememberMe'); ?>
+		<?php echo $form->label($model,'No cerrar sesión'); ?>
 		<?php echo $form->error($model,'rememberMe'); ?>
 	</div>
-
+	
 	<div class="row buttons">
 		<?php echo CHtml::submitButton('Login'); ?>
-		
-	
-
 	</div>
 
 <?php $this->endWidget(); ?>
 </div><!-- form -->
+</div> <!--Termino del col-->
+<div class="col-xs-6 col-sm-4"></div>
+</div> <!--Termino del row-->
+
