2bcebf887b7b7279c84c58550b8c77a70757584c
Pablo Morales Alarcón <pamoral@alumnos.ubiobio.cl>
2014-11-14 16:07:02 -0300

    UserIdentify
    comente y pegue lo del SAPP

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -32,3 +32,35 @@ class UserIdentity extends CUserIdentity
 		return !$this->errorCode;
 	}
 }
+
+/*   LO DEL RUBEN
+	<?php
+class UserIdentity extends CUserIdentity 
+{
+    private $_id;  //lo ocupa para guaradr el username. (11.111.111-1)
+    public function authenticate()
+    {
+        $user=UsuLogin::model()->findByAttributes(array('username'=>$this->username)); // crea la vista de todos los usuarios login
+        if($user===null)
+            $this->errorCode=self::ERROR_USERNAME_INVALID;
+        else if(!$user->validatePassword($this->password))
+            $this->errorCode=self::ERROR_PASSWORD_INVALID;
+        else
+        {
+            $this->_id=$user->username; //_id le pasa el username (admin)
+            $this->username=$user->PER_ROLE; //y lo que tenia username, le pasa la variable ROL. (NOMBRE).
+            $this->setState('role', $user->PER_ROLE);
+            $this->setState('nombre',$user->PER_NOMBRE);
+            $this->setState('carrera',$user->CAR_CODIGO);
+            $this->setState('ID',$user->PER_ID);
+            $this->errorCode=self::ERROR_NONE;
+        }
+        return $this->errorCode==self::ERROR_NONE;
+    }
+    
+    public function getId()
+    {
+        return $this->_id;
+    }
+}
+*/
