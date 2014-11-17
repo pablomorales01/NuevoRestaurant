142d75b481b4e1d5bf5649a61ac017b48f92cd2d
elymarquez <marquezneira.e@gmail.com>
2014-11-17 14:55:28 -0300

    FUCCCCCCCCCCCCCCCCK!

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -1,58 +1,29 @@
+
 <?php
-
-/**
- * UserIdentity represents the data needed to identity a user.
- * It contains the authentication method that checks if the provided
- * data can identity the user.
- */
-class UserIdentity extends CUserIdentity
-{
-	/**
-	 * Authenticates a user.
-	 * The example implementation makes sure if the username and password
-	 * are both 'demo'.
-	 * In practical applications, this should be changed to authenticate
-	 * against some persistent user identity storage (e.g. database).
-	 * @return boolean whether authentication succeeds.
-	 */
-	public function authenticate()
-	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			//11.111.111-1
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
-			$this->errorCode=self::ERROR_NONE;
-		return !$this->errorCode;
-	}
-}
-
-/*   LO DEL RUBEN
-	<?php
 class UserIdentity extends CUserIdentity 
 {
-    private $_id;  //lo ocupa para guaradr el username. (11.111.111-1)
+    private $_id;
     public function authenticate()
     {
-        $user=UsuLogin::model()->findByAttributes(array('username'=>$this->username)); // crea la vista de todos los usuarios login
+        $user=UsuariosLogin::model()->findByAttributes(array('username'=>$this->username));
+
         if($user===null)
             $this->errorCode=self::ERROR_USERNAME_INVALID;
-        else if(!$user->validatePassword($this->password))
+
+        elseif ($user->password != $this->password)
             $this->errorCode=self::ERROR_PASSWORD_INVALID;
+       /* else if(!$user->validatePassword($this->password))
+            $this->errorCode=self::ERROR_PASSWORD_INVALID;*/
+
         else
         {
-            $this->_id=$user->username; //_id le pasa el username (admin)
-            $this->username=$user->PER_ROLE; //y lo que tenia username, le pasa la variable ROL. (NOMBRE).
-            $this->setState('role', $user->PER_ROLE);
-            $this->setState('nombre',$user->PER_NOMBRE);
-            $this->setState('carrera',$user->CAR_CODIGO);
-            $this->setState('ID',$user->PER_ID);
+            $this->_id=$user->username;
+            $this->username=$user->ROLNOMBRE;
+
+            $this->setState('ROL', $user->ROLNOMBRE);
+            $this->setState('NOMBRES',$user->USUNOMBRES);
+            $this->setState('APELLIDOS', $user->USUAPELLIDOS);
+            $this->setState('ID', $user->USU_ID);
             $this->errorCode=self::ERROR_NONE;
         }
         return $this->errorCode==self::ERROR_NONE;
@@ -61,11 +32,5 @@ class UserIdentity extends CUserIdentity
     public function getId()
     {
         return $this->_id;
-<<<<<<< HEAD
-    }
-}
-*/
-=======
     }    
 }
->>>>>>> origin/Elizabeth
