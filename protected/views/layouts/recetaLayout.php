7aebeb2886ac9e049868111fbb5af67da4ab06d0
elymarquez <marquezneira.e@gmail.com>
2015-03-27 01:14:59 -0300

    LISTO! :D <3 :baby_symbol:

---
+++ protected/views/layouts/recetaLayout.php
@@ -0,0 +1,15 @@
+<?php $this->beginContent('//layouts/main'); ?>
+
+<div class="row"> 
+  <div class="col-xs-12 col-sm-6 col-md-8">
+  <?php  echo $content;?>
+  </div>
+  <div class="col-xs-6 col-md-4">
+  <br>
+    <ul class="nav nav-pills nav-stacked">
+        <li><a href="<?php echo Yii::app()->createUrl('receta/create'); ?>">Nuevo plato preparado</a></li>
+        <li><a href="<?php echo Yii::app()->createUrl('receta/admin'); ?>">Administrar preparaciones</a></li>
+  </div>
+</div>
+
+<?php $this->endContent(); ?>
