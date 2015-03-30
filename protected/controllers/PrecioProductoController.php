74981618622aa18d15f6b2426a7c2363b7071bce
elymarquez <marquezneira.e@gmail.com>
2015-03-30 19:36:23 -0300

    admin cocina

--- protected/controllers/PrecioProductoController.php
+++ protected/controllers/PrecioProductoController.php
@@ -6,7 +6,7 @@ class PrecioProductoController extends Controller
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
 	 * using two-column layout. See 'protected/views/layouts/column2.php'.
 	 */
-	public $layout='//layouts/menulayout';
+	public $layout='//layouts/precioProductoLayout';
 
 	/**
 	 * @return array action filters
