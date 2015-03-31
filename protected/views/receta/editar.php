f5517080342a390a67db22164cea0712d09513fb
elymarquez <marquezneira.e@gmail.com>
2015-03-31 18:20:56 -0300

    :panda_face:

--- protected/views/receta/editar.php
+++ protected/views/receta/editar.php
@@ -1,11 +1,9 @@
+<h1 align="center">Modificar Receta </h1>
+
 <div class="form">
 
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'receta-form',
-	// Please note: When you enable ajax validation, make sure the corresponding
-	// controller action is handling ajax validation correctly.
-	// There is a call to performAjaxValidation() commented in generated controller code.
-	// See class documentation of CActiveForm for details on this.
 	'enableAjaxValidation'=>true,)); 
 
 $form = $this->beginWidget('bootstrap.widgets.BsActiveForm', array(
