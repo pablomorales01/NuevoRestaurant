621b0fd76373d8ac42f0b2267a6a19afb026fbeb
Pablo Morales Alarcón <pamoral@alumnos.ubiobio.cl>
2015-03-30 21:47:07 -0300

    Revert "admin cocina"
    This reverts commit 74981618622aa18d15f6b2426a7c2363b7071bce.

--- protected/controllers/PrecioProductoController.php
+++ protected/controllers/PrecioProductoController.php
@@ -6,7 +6,7 @@ class PrecioProductoController extends Controller
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
 	 * using two-column layout. See 'protected/views/layouts/column2.php'.
 	 */
-	public $layout='//layouts/precioProductoLayout';
+	public $layout='//layouts/menulayout';
 
 	/**
 	 * @return array action filters
