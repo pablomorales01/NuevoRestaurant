95e453e17161bdc6c0df19862c97662278e76042
elymarquez <marquezneira.e@gmail.com>
2015-04-15 18:27:45 -0300

    admin comanda

--- protected/views/comanda/admin.php
+++ protected/views/comanda/admin.php
@@ -1,29 +1,81 @@
 <?php
 /* @var $this ComandaController */
 /* @var $model Comanda */
-
+$mesa =0;
+//cero no repetido
+$repetido =0;
 ?>
 
 <h1 align="center">Administrar Comandas</h1>
+<br>
+<table >
+	<tr>
+		<th>Mesa</th>
+		<th>Fecha</th>
+		<th>Menu</th>
+		<th>Cantidad</th>
+		<th>Opciones</th>
+	</tr>
+	<?php foreach ($comandas as $com) {
+		if($com->MESANUM != $mesa){
+			$mesa = $com->MESANUM;
+			$repetido =0;
+			?>	
+			<tr>
+				<td><?php echo $mesa;?></td>
+				<td><?php echo $com->COMFECHA;?></td>
+		<?php } 
+		else {?>
+				<td></td>
+				<td></td>
+				<?php  
+				$repetido = 1;
+			}?>
+			<?php $menu = ListaDePrecios::model()->findByAttributes(array('RESTO_ID'=>Yii::app()->user->RESTAURANT,
+			'MENU_ID'=>$com->MENU_ID)); ?>
+			<td><?php echo $menu->MENUNOMBRE;?></td>
+			<td><?php echo $com->COM_CANTIDAD; ?></td>	
+			<?php if($repetido == 0){ ?>
+			<td>
+			<div class="btn-group">
+				<div class="input-group">
+					<button type="button" class="btn btn-info btn-sm dropdown-toggle" data-toggle="dropdown">
+						<span class="glyphicon glyphicon-cog"></span>
+					</button>
 
+					<ul class="dropdown-menu pull-right">
+						<li> 
+							<a href="<?php echo Yii::app()->createUrl("comanda/update/$com->MESANUM"); ?>">Editar</a>
+						</li>
+						<li > 
+							<li data-toggle="modal" data-target="#questionDelete<?php echo $com->MESANUM?>"><a>Eliminar</a></li>
 
-<?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'comanda-grid',
-	'dataProvider'=>$model->search(),
-	'filter'=>$model,
-	'columns'=>array(
-		'COM_ID',
-		'VENTA_ID',
-		'MENU_ID',
-		'MESANUM',
-		'USU_ID',
-		'USU_USU_ID',
-		/*
-		'COMFECHA',
-		'COM_ESTADO',
-		*/
-		array(
-			'class'=>'CButtonColumn',
-		),
-	),
-)); ?>
+						</li>
+					</ul>
+					
+				</div> 
+			</div>
+
+			<div class="modal fade" id="questionDelete<?php echo $com->MESANUM?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+							<h4 class="modal-title">Eliminar</h4>
+						</div>
+						<div class="modal-body">
+							¿Desea realmente eliminar?
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+							<button type="button" class="btn btn-danger" onclick="location.href='<?php echo Yii::app()->createUrl("comanda/delete/$com->MESANUM"); ?>'">Eliminar </button>
+						</div>
+					</div>
+				</div>
+			</div>
+			</td>
+			<?php } ?> <!-- END IF REPETIDO -->
+			</tr> <!-- FILA -->
+		<!-- END FOREACH -->
+<?php }?>
+	</table>
