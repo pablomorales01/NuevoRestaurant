373cd9fb473b30cce2d6ab0b311cea4975804451
Pablo Morales Alarcón <pamoral@alumnos.ubiobio.cl>
2015-04-22 14:37:44 -0300

    fecha: desde, hasta

--- protected/views/venta/admin.php
+++ protected/views/venta/admin.php
@@ -1,32 +1,55 @@
 <?php
 /* @var $this VentaController */
 /* @var $model Venta */
-Yii::app()->clientScript->registerScript('search', "
-$('.search-button').click(function(){
-	$('.search-form').toggle();
-	return false;
-});
-$('.search-form form').submit(function(){
-	$('#venta-grid').yiiGridView('update', {
-		data: $(this).serialize()
-	});
-	return false;
-});
-");
 ?>
 
 <h1 align="center">Administrar Ventas</h1>
 
-<?php echo CHtml::link('Busqueda Avanzada','#',array('class'=>'search-button')); ?>
-<div class="search-form" style="display:none">
-<?php $this->renderPartial('_search',array(
-	'model'=>$model,
-)); ?>
-</div><!-- search-form -->
+<?php
+// this is the date picker
+$dateisOn = $this->widget('zii.widgets.jui.CJuiDatePicker', array(
+ /*'model'=>$model,
+ 'attribute' => 'date_first',*/
+ 'name' => 'Venta[fecha1]',
+ 'value' => $model->fecha1,
+ 'language'=>'es',
+ 'options'=>array(
+ 'showAnim'=>'fold',
+ 'dateFormat'=>'yy-mm-dd',
+ 'changeMonth' => 'true',
+ 'changeYear'=>'true',
+ 'constrainInput' => 'false',
+ ),
+ 'htmlOptions'=>array(
+ 'style'=>'height:20px;width:70px;',
+ ),
+ ),true) . '<br> hasta <br> ' . $this->widget('zii.widgets.jui.CJuiDatePicker', array(
+ /*'model'=>$model,
+ 'attribute' => 'date_last',*/
+ 'name' => 'Venta[fecha2]',
+ 'value' => $model->fecha2,
+ 'language'=>'es',
+ 'options'=>array(
+ 'showAnim'=>'fold',
+ 'dateFormat'=>'yy-mm-dd',
+ 'changeMonth' => 'true',
+ 'changeYear'=>'true',
+ 'constrainInput' => 'false',
+ ),
+ 'htmlOptions'=>array(
+ 'style'=>'height:20px;width:70px',
+ ),
+ ),true);
+?>
+
 
 <?php $this->widget('zii.widgets.grid.CGridView', array(
 	'id'=>'venta-grid',
 	'dataProvider'=>$model->search(),
+	'afterAjaxUpdate'=>"function() { //Esto es lo que hay que agregar
+	 jQuery('#Venta_fecha1').datepicker(jQuery.extend({showMonthAfterYear:false}, jQuery.datepicker.regional['es'], {'showAnim':'fold','dateFormat':'yy-mm-dd','changeMonth':'true','showButtonPanel':'true','changeYear':'true','constrainInput':'false'}));
+	 jQuery('#Venta_fecha2').datepicker(jQuery.extend({showMonthAfterYear:false}, jQuery.datepicker.regional['es'], {'showAnim':'fold','dateFormat':'yy-mm-dd','changeMonth':'true','showButtonPanel':'true','changeYear':'true','constrainInput':'false'}));
+	 }",
 	'filter'=>$model,
 	'columns'=>array(
 		'VENTA_ID',
@@ -34,7 +57,9 @@ $('.search-form form').submit(function(){
 			  'value'=>'$data->uSU->USUNOMBRES'),
 		array('name'=>'USU_ID',
 			  'value'=>'$data->uSU->USUAPELLIDOS'),
-		'VENTAFECHA',
+		array('name'=>'VENTAFECHA',
+			  'filter'=>$dateisOn,
+			  'value'=>'$data->VENTAFECHA'),
 		'VENTATOTAL',
 		'VENTAFORMADEPAGO',
 		array(
