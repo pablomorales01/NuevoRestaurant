0ab1f57321ca452e887135ffc4e975f7aea3cf01
Pablo Morales Alarcón <pamoral@alumnos.ubiobio.cl>
2015-04-22 13:40:19 -0300

    search

--- protected/views/venta/_search.php
+++ protected/views/venta/_search.php
@@ -12,32 +12,12 @@
 )); ?>
 
 	<div class="row">
-		<?php echo $form->label($model,'VENTA_ID'); ?>
-		<?php echo $form->textField($model,'VENTA_ID'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'USU_ID'); ?>
-		<?php echo $form->textField($model,'USU_ID'); ?>
-	</div>
-
-	<div class="row">
 		<?php echo $form->label($model,'VENTAFECHA'); ?>
 		<?php echo $form->textField($model,'VENTAFECHA'); ?>
 	</div>
 
-	<div class="row">
-		<?php echo $form->label($model,'VENTATOTAL'); ?>
-		<?php echo $form->textField($model,'VENTATOTAL'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'VENTAFORMADEPAGO'); ?>
-		<?php echo $form->textField($model,'VENTAFORMADEPAGO',array('size'=>15,'maxlength'=>15)); ?>
-	</div>
-
 	<div class="row buttons">
-		<?php echo CHtml::submitButton('Search'); ?>
+		<?php echo CHtml::submitButton('Buscar'); ?>
 	</div>
 
 <?php $this->endWidget(); ?>
--- protected/views/venta/admin.php
+++ protected/views/venta/admin.php
@@ -1,10 +1,28 @@
 <?php
 /* @var $this VentaController */
 /* @var $model Venta */
-
+Yii::app()->clientScript->registerScript('search', "
+$('.search-button').click(function(){
+	$('.search-form').toggle();
+	return false;
+});
+$('.search-form form').submit(function(){
+	$('#venta-grid').yiiGridView('update', {
+		data: $(this).serialize()
+	});
+	return false;
+});
+");
 ?>
 
 <h1 align="center">Administrar Ventas</h1>
+
+<?php echo CHtml::link('Busqueda Avanzada','#',array('class'=>'search-button')); ?>
+<div class="search-form" style="display:none">
+<?php $this->renderPartial('_search',array(
+	'model'=>$model,
+)); ?>
+</div><!-- search-form -->
 
 <?php $this->widget('zii.widgets.grid.CGridView', array(
 	'id'=>'venta-grid',
